299206bbcf82b5aa4e3f21fe3d9b5e5d82a60d89
meamarp <appotdar05@gmail.com>
2022-03-06 15:01:13 +0530

    adds method to compute MSE

--- mlutils.py
+++ mlutils.py
@@ -3,6 +3,7 @@ import numpy as np
 def accuracy(y_true, y_pred):
     return np.sum(y_true == y_pred) / len(y_true)
 
-
+def mean_squrd_error(y_true, y_pred):
+    return np.mean( (y_true - y_pred) ** 2)
 
 
